c17e21512040d3e7272e5b39a0bf1cf46d27f11c
VASU CHAND <vasuchand9@gmail.com>
2026-02-09 09:23:57 +0530

    add

--- day6/qs1.c
+++ day6/qs1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int fib(int n) {
+int fib(int n)
+{
     if (n == 0)
         return 0;
     if (n == 1)
@@ -8,7 +9,8 @@ int fib(int n) {
     return fib(n - 1) + fib(n - 2);
 }
 
-int main() {
+int main()
+{
     int n;
     scanf("%d", &n);
     printf("%d", fib(n));
